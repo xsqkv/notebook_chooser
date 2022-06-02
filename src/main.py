159aa0a40307c8694b3002450fa9935f9d7f7148
Artem Vanty <artem.vanty.0358@gmail.com>
2022-06-02 20:38:17 +0900

    Complete for Linux

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-import os,sys,string
+'''
+Program wich helps you choose notebook for work with mathemathic packets
 
+Author: Artem Vanty GitHub:vantyartem
+'''
+import os,sys
 import termios, fcntl
 import select
-from time import sleep
+import http.client
+import re
 
-import types
-import vars
 from term import *
-import tui
-
-
 
 sign =[r"Добро пожаловать в программу",
        r"по выбору оптимальной конфигурации для ноутбука",
@@ -32,20 +32,18 @@ sign =[r"Добро пожаловать в программу",
 width = os.get_terminal_size().columns
 height = os.get_terminal_size().lines
 
-money=0 #
+money=0 #budget
 prior=0 #gc or processor
 storage=0 #ssd
-
-#                                                            proc        ram         card       storage
-#https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?f[66c]=267w&f[670]=26nn&f[67l]=26t8&f[qe6o]=10jmye
-choose_processor = [['267w','267z','2680','2681'],
-['Intel Athlon','i3','i5','i7']]
-choose_ram_type = [['26nn','9wjk','9wjk','1cf1vp'],
-['DDR3','DDR4','DDR4','DDR5']]
-choose_graphic_card = [['26t8','l5bba','14f68l','yurrt'],
-['Integrated','GTX 1650','RTX 3060','RTX 3080']]
-choose_storage_type = [['10jmye','26x7','26x7','26xr'],
-['emmc 32gb','hdd 1000gb','hdd 1000gb','ssd 512']]
+#https://www.citilink.ru/catalog/noutbuki/?sorting=price_asc&f=277_3pentium,1046_3integrirovannyy,11496_3,11699_3ddr3
+choose_processor = [['277_3pentium','277_3ryzend15','277_3cored1i5','277_3cored1i7'],
+['Pentium','ryzen 5','i5','i7']]
+choose_ram_type = [['11699_3ddr3','11699_3ddr4','11699_3ddr4','11699_3ddr4'],
+['DDR3','DDR4','DDR4','DDR4']]
+choose_graphic_card = [['1046_3integrirovannyy','1046_3integrirovannyy','12461_3nvidiad1geforced1d1rtxd13060d1dlyad1noutbukovd1a5d16144d1mb','12461_3nvidiad1geforced1d1rtxd13070d1dlyad1noutbukovd1a5d18192d1mb'],
+['Integrated','Integrated','RTX 3060','RTX 3070']]
+choose_storage_type = [['11496_3','2588_3','2589_3','2589_3'],
+['emmc','hdd','ssd','ssd']]
 
 #             cheap     medium     uni     powerfull
 processor = ['ryzen 3','ryzen 5','ryzen 7','ryzen 9']
@@ -56,10 +54,10 @@ ram_type = ['ddr2','ddr3','ddr4','ddr5']
 storage_type = [['emmc','hdd','ssd','ssd'],
 ['emmc','hdd','ssd','nvme']]
 notebook_type = ['Дешёвый ноутбук','Бюджетный ноутбук','Универсальный ноутбук','Мощный ноутбук']
-
+#Clear fucntion
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-
+#Set text by x and y
 def settext(text,x,y,color=colors.white,style=styles.none,lst='\n'):
     out("\033[%d;%dH" % (y,x) + text,color,style,lst)
 
@@ -73,10 +71,10 @@ def constructor():
     cls()
     settext('Какой у вас бюджет?',width/2-10,height/3)
     settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-    settext('До 15.000',width/2-5,height/2)
-    settext('15.000 - 30.000',width/2-8,height/2+2)
-    settext('30.000 - 70.000',width/2-8,height/2+4)
-    settext('70.000+',width/2-4,height/2+6,style=styles.inverse)
+    settext('До 25.000',width/2-5,height/2)
+    settext('25.000 - 40.000',width/2-8,height/2+2)
+    settext('40.000 - 90.000',width/2-8,height/2+4)
+    settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
     
     idx = 10004
 
@@ -94,34 +92,34 @@ def constructor():
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6,style=styles.inverse)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
         elif idx%4==3:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Универсальный ноутбук',width/2-11,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4,style=styles.inverse)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4,style=styles.inverse)
+            settext('90.000+',width/2-4,height/2+6)
         elif idx%4==2:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Бюджетный ноутбук',width/2-9,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2,style=styles.inverse)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2,style=styles.inverse)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6)
         elif idx%4==1:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Дешёвый ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2,style=styles.inverse)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2,style=styles.inverse)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6)
     
     idx = 10001
 
@@ -193,10 +191,10 @@ def choose():
     cls()
     settext('Какой у вас бюджет?',width/2-10,height/3)
     settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-    settext('До 15.000',width/2-5,height/2)
-    settext('15.000 - 30.000',width/2-8,height/2+2)
-    settext('30.000 - 70.000',width/2-8,height/2+4)
-    settext('70.000+',width/2-4,height/2+6,style=styles.inverse)
+    settext('До 25.000',width/2-5,height/2)
+    settext('25.000 - 40.000',width/2-8,height/2+2)
+    settext('40.000 - 90.000',width/2-8,height/2+4)
+    settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
     
     idx = 10004
 
@@ -214,99 +212,49 @@ def choose():
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6,style=styles.inverse)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
         elif idx%4==3:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Универсальный ноутбук',width/2-11,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4,style=styles.inverse)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4,style=styles.inverse)
+            settext('90.000+',width/2-4,height/2+6)
         elif idx%4==2:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Бюджетный ноутбук',width/2-9,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2)
-            settext('15.000 - 30.000',width/2-8,height/2+2,style=styles.inverse)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2)
+            settext('25.000 - 40.000',width/2-8,height/2+2,style=styles.inverse)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6)
         elif idx%4==1:
             cls()
             settext('Какой у вас бюджет?',width/2-10,height/3)
             settext('Дешёвый ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 15.000',width/2-5,height/2,style=styles.inverse)
-            settext('15.000 - 30.000',width/2-8,height/2+2)
-            settext('30.000 - 70.000',width/2-8,height/2+4)
-            settext('70.000+',width/2-4,height/2+6)
+            settext('До 25.000',width/2-5,height/2,style=styles.inverse)
+            settext('25.000 - 40.000',width/2-8,height/2+2)
+            settext('40.000 - 90.000',width/2-8,height/2+4)
+            settext('90.000+',width/2-4,height/2+6)
     
-    idx = 10001
-
-    cls()
-    settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-    settext('Графическая карта даёт ощутимый прирост производительности и позволяет производить графические вычисления',width/2-53,height/3+(height/2-height/3)-3)
-    settext('Процессор',width/2-5,height/2)
-    settext('Графическая карта',width/2-9,height/2+2,style=styles.inverse)
-
-    while True:
-        inp, outp, err = select.select([sys.stdin], [], [])
-        c = sys.stdin.read()
-        if c == '\033[A': #UP ARROW
-            idx-=1
-        elif c == '\033[B': #DOWN ARROW
-            idx+=1
-        elif c == '\n': # ENTER KEY
-            prior=idx%2
-            break
-        if idx%2==1:
-            cls()
-            settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-            settext('Графическая карта даёт ощутимый прирост производительности и позволяет производить графические вычисления',width/2-53,height/3+(height/2-height/3)-3)
-            settext('Процессор',width/2-5,height/2)
-            settext('Графическая карта',width/2-9,height/2+2,style=styles.inverse)
-        else:
-            cls()
-            settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-            settext('Процессор позволяет производить базовые операции и является сердцем компьютера',width/2-39,height/3+(height/2-height/3)-3)
-            settext('Процессор',width/2-5,height/2,style=styles.inverse)
-            settext('Графическая карта',width/2-9,height/2+2)
-    
-    idx = 10001
-
-    cls()
-    settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-    settext('Нет',width/2-1,height/2)
-    settext('Да',width/2-1,height/2+2,style=styles.inverse)
-
-    while True:
-        inp, outp, err = select.select([sys.stdin], [], [])
-        c = sys.stdin.read()
-        if c == '\033[A': #UP ARROW
-            idx-=1
-        elif c == '\033[B': #DOWN ARROW
-            idx+=1
-        elif c == '\n': # ENTER KEY
-            storage=idx%2
-            break
-        if idx%2==1:
-            cls()
-            settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-            settext('Нет',width/2-1,height/2)
-            settext('Да',width/2-1,height/2+2,style=styles.inverse)
-        else:
-            cls()
-            settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-            settext('Нет',width/2-1,height/2,style=styles.inverse)
-            settext('Да',width/2-1,height/2+2)
+    indent = 0
     money -= 1
     if money < 0 : money = 3
     cls()
     settext('Вам подойдёт:',width/2-6,height/2-3,style=styles.bold)
-    st = f'\033[1m{notebook_type[money]}\033[0m Процессор: {choose_processor[1][money]} Видеокарта: {choose_graphic_card[1][money]} Тип ОЗУ: {choose_ram_type[1][money]} Тип хранилища: {choose_storage_type[1][money]}'
-    settext(st,width/2-len(st)/2,height/2)
+    c = http.client.HTTPSConnection('www.citilink.ru')
+    c.request('GET',f'/catalog/noutbuki/?sorting=price_asc&f={choose_processor[0][money]},{choose_graphic_card[0][money]},{choose_storage_type[0][money]},{choose_ram_type[0][money]}')
+    r = c.getresponse()
+    html = r.read().decode('UTF-8')
+    result = re.findall(r'alt=".+',html)
+    for x in result[:-1]:
+        st = f'\033[1m{x[5:-1]}\033[0m Процессор: {choose_processor[1][money]} Видеокарта: {choose_graphic_card[1][money]} Тип ОЗУ: {choose_ram_type[1][money]} Тип хранилища: {choose_storage_type[1][money]}'
+        settext(st,width/2-len(st)/2,height/2+indent)
+        indent += 1
     setoldflags()
 
 fd = sys.stdin.fileno()
