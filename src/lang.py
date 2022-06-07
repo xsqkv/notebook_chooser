ef9538e2980c7140eb1ccb320f29cb8f05ed8806
Artem Vanty <artem.vanty.0358@gmail.com>
2022-06-08 01:32:19 +0900

    add language choice

---
+++ src/lang.py
@@ -0,0 +1,36 @@
+budget = ['Какой у вас бюджет?','What\'s your budget?']
+powerful_notebook = ['Мощный ноутбук','Powerful laptop']
+twenty_five = ['До 25.000','Up to 25.000']
+universal_notebook = ['Универсальный ноутбук','Universal Notebook']
+budget_notebook = ['Бюджетный ноутбук','Budget laptop']
+cheap_notebook = ['Дешёвый ноутбук','Cheap laptop']
+
+
+ops_prior = ['Какого вида вычисления в приоритете?','What kind of calculation has priority?']
+gc = ['Графическая карта даёт ощутимый прирост производительности и позволяет производить графические вычисления','The graphics card gives a noticeable performance boost and allows for graphical computing']
+procc = ['Процессор позволяет производить базовые операции и является сердцем компьютера','The processor allows you to perform basic operations and is the heart of the computer']
+proc = ['Процессор','CPU']
+gh = ['Графическая карта','Graphic card']
+
+
+fast_files = ['Требуется ли быстрая работа с файлами?','Do you need fast file handling?']
+yes = ['Нет','No']
+no = ['Да','Yes']
+
+
+suit = ['Вам подойдёт:','Suitable for:']
+prc = ['Процессор','CPU']
+graphic_c = ['Видеокарта','Graphic card']
+ram_t = ['Тип ОЗУ','RAM type']
+storage_t = ['Тип хранилища','Storage type']
+
+
+exitt = ['Выход','Exit']
+nextt = ['Далее','Next']
+
+
+select_mode = ['Выберите режим','Select mode']
+chose = ['Подборка','Selection']
+chose_desc = ['\x1b[1mПодборка\x1b[0m - этот режим предназначен для выбора ноутбука из уже существующих моделей в сети интернет','\x1b[1mSelection\x1b[0m - this mode is designed to select a laptop from existing models on the Internet']
+construct = ['Конструктор','Constructor']
+construct_desc = ['\x1b[1mКонструктор\x1b[0m - этот режим предназначен для создания своих параметров для ноутбука','\x1b[1mConstructor\x1b[0m - this mode is for creating custom parameters for a laptop']
--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ else:
 
 from term import *
 from vars import *
-
-
+from lang import *
+lang = 0
 #if unix system then include this code
 if os.name != 'nt':
     fd = sys.stdin.fileno()
@@ -46,28 +46,27 @@ def getchar():
         inp, outp, err = select.select([sys.stdin], [], [])
         c = sys.stdin.read()
     return c
-
 #Clear function
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 #Set text by x and y
 def settext(text,x,y,color=colors.white,style=styles.none,lst='\n'):
-    out("\x1b[%d;%dH" % (y,x) + text,color,style,lst)
+    out("\x1b[%d;%dH" % (y,x-len(text)/2) + text,color,style,lst)
 #set sign on the screen
 def setsign(color=colors.white,style=styles.none,lst='\n'):
     i = 0
-    for x in sign:
-        settext(x,width/2 - len(x)/2,height/3 - len(sign)/2 + i,color,style,lst)
+    for x in sign[lang]:
+        settext(x,width/2,height/3 - len(sign[lang])/2 + i,color,style,lst)
         i+=1
 #Constructor page
 def constructor():
     cls()#clear screen
-    settext('Какой у вас бюджет?',width/2-10,height/3)
-    settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-    settext('До 25.000',width/2-5,height/2)
-    settext('25.000 - 40.000',width/2-8,height/2+2)
-    settext('40.000 - 90.000',width/2-8,height/2+4)
-    settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
+    settext(budget[lang],width/2,height/3)
+    settext(powerful_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+    settext(twenty_five[lang],width/2,height/2)
+    settext('25.000 - 40.000',width/2,height/2+2)
+    settext('40.000 - 90.000',width/2,height/2+4)
+    settext('90.000+',width/2,height/2+6,style=styles.inverse)
     
     idx = 10004 #index of cursor
 
@@ -83,44 +82,44 @@ def constructor():
             break
         if idx%4==0:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
+            settext(budget[lang],width/2,height/3)
+            settext(powerful_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6,style=styles.inverse)
         elif idx%4==3:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Универсальный ноутбук',width/2-11,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4,style=styles.inverse)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(universal_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4,style=styles.inverse)
+            settext('90.000+',width/2,height/2+6)
         elif idx%4==2:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Бюджетный ноутбук',width/2-9,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2,style=styles.inverse)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(budget_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2,style=styles.inverse)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6)
         elif idx%4==1:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Дешёвый ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2,style=styles.inverse)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(cheap_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2,style=styles.inverse)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6)
     
     idx = 10001 #index of cursor
 
     cls()#clear screen
-    settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-    settext('Графическая карта даёт ощутимый прирост производительности и позволяет производить графические вычисления',width/2-53,height/3+(height/2-height/3)-3)
-    settext('Процессор',width/2-5,height/2)
-    settext('Графическая карта',width/2-9,height/2+2,style=styles.inverse)
+    settext(ops_prior[lang],width/2,height/3)
+    settext(gc[lang],width/2,height/3+(height/2-height/3)-3)
+    settext(proc[lang],width/2,height/2)
+    settext(gh[lang],width/2,height/2+2,style=styles.inverse)
 
     while True:
         
@@ -134,23 +133,23 @@ def constructor():
             break
         if idx%2==1:
             cls()#clear screen
-            settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-            settext('Графическая карта даёт ощутимый прирост производительности и позволяет производить графические вычисления',width/2-53,height/3+(height/2-height/3)-3)
-            settext('Процессор',width/2-5,height/2)
-            settext('Графическая карта',width/2-9,height/2+2,style=styles.inverse)
+            settext(ops_prior[lang],width/2,height/3)
+            settext(gc[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(proc[lang],width/2,height/2)
+            settext(gh[lang],width/2,height/2+2,style=styles.inverse)
         else:
             cls()#clear screen
-            settext('Какого вида вычисления в приоритете?',width/2-18,height/3)
-            settext('Процессор позволяет производить базовые операции и является сердцем компьютера',width/2-39,height/3+(height/2-height/3)-3)
-            settext('Процессор',width/2-5,height/2,style=styles.inverse)
-            settext('Графическая карта',width/2-9,height/2+2)
+            settext(ops_prior[lang],width/2,height/3)
+            settext(procc[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(proc[lang],width/2,height/2,style=styles.inverse)
+            settext(gh[lang],width/2,height/2+2)
     
     idx = 10001 #index of cursor
 
     cls()#clear screen
-    settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-    settext('Нет',width/2-1,height/2)
-    settext('Да',width/2-1,height/2+2,style=styles.inverse)
+    settext(fast_files[lang],width/2,height/3)
+    settext(yes[lang],width/2,height/2)
+    settext(no[lang],width/2,height/2+2,style=styles.inverse)
 
     while True:
         
@@ -164,30 +163,30 @@ def constructor():
             break
         if idx%2==1:
             cls()#clear screen
-            settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-            settext('Нет',width/2-1,height/2)
-            settext('Да',width/2-1,height/2+2,style=styles.inverse)
+            settext(fast_files[lang],width/2,height/3)
+            settext(yes[lang],width/2,height/2)
+            settext(no[lang],width/2,height/2+2,style=styles.inverse)
         else:
             cls()#clear screen
-            settext('Требуется ли быстрая работа с файлами?',width/2-18,height/3)
-            settext('Нет',width/2-1,height/2,style=styles.inverse)
-            settext('Да',width/2-1,height/2+2)
+            settext(fast_files[lang],width/2,height/3)
+            settext(yes[lang],width/2,height/2,style=styles.inverse)
+            settext(no[lang],width/2,height/2+2)
     money -= 1
     if money < 0 : money = 3
     cls()#clear screen
-    settext('Вам подойдёт:',width/2-6,height/2-3,style=styles.bold)
-    st = f'\x1b[1m{notebook_type[money]}\x1b[0m Процессор: {processor[money]} Видеокарта: {graphic_card[prior][money]} Тип ОЗУ: {ram_type[money]} Тип хранилища: {storage_type[storage][money]}'
-    settext(st,width/2-len(st)/2,height/2)
+    settext(suit[lang],width/2,height/2-3,style=styles.bold)
+    st = f'\x1b[1m{notebook_type[lang][money]}\x1b[0m {prc[lang]}: {processor[money]} {graphic_c[lang]}: {graphic_card[prior][money]} {ram_t[lang]}: {ram_type[money]} {storage_t[lang]}: {storage_type[storage][money]}'
+    settext(st,width/2,height/2)
     setoldflags()#set old flags
 #Choose page
 def choose():
     cls()#clear screen
-    settext('Какой у вас бюджет?',width/2-10,height/3)
-    settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-    settext('До 25.000',width/2-5,height/2)
-    settext('25.000 - 40.000',width/2-8,height/2+2)
-    settext('40.000 - 90.000',width/2-8,height/2+4)
-    settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
+    settext(budget[lang],width/2,height/3)
+    settext(powerful_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+    settext(twenty_five[lang],width/2,height/2)
+    settext('25.000 - 40.000',width/2,height/2+2)
+    settext('40.000 - 90.000',width/2,height/2+4)
+    settext('90.000+',width/2,height/2+6,style=styles.inverse)
     
     idx = 10004 #index of cursor
 
@@ -203,42 +202,42 @@ def choose():
             break
         if idx%4==0:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Мощный ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6,style=styles.inverse)
+            settext(budget[lang],width/2,height/3)
+            settext(powerful_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6,style=styles.inverse)
         elif idx%4==3:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Универсальный ноутбук',width/2-11,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4,style=styles.inverse)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(universal_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4,style=styles.inverse)
+            settext('90.000+',width/2,height/2+6)
         elif idx%4==2:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Бюджетный ноутбук',width/2-9,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2)
-            settext('25.000 - 40.000',width/2-8,height/2+2,style=styles.inverse)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(budget_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2)
+            settext('25.000 - 40.000',width/2,height/2+2,style=styles.inverse)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6)
         elif idx%4==1:
             cls()#clear screen
-            settext('Какой у вас бюджет?',width/2-10,height/3)
-            settext('Дешёвый ноутбук',width/2-8,height/3+(height/2-height/3)-3)
-            settext('До 25.000',width/2-5,height/2,style=styles.inverse)
-            settext('25.000 - 40.000',width/2-8,height/2+2)
-            settext('40.000 - 90.000',width/2-8,height/2+4)
-            settext('90.000+',width/2-4,height/2+6)
+            settext(budget[lang],width/2,height/3)
+            settext(cheap_notebook[lang],width/2,height/3+(height/2-height/3)-3)
+            settext(twenty_five[lang],width/2,height/2,style=styles.inverse)
+            settext('25.000 - 40.000',width/2,height/2+2)
+            settext('40.000 - 90.000',width/2,height/2+4)
+            settext('90.000+',width/2,height/2+6)
     
     indent = 0
     money -= 1
     if money < 0 : money = 3
     cls()#clear screen
-    settext('Вам подойдёт:',width/2-6,height/2-3,style=styles.bold)
+    settext(suit[lang],width/2,height/2-3,style=styles.bold)
     c = http.client.HTTPSConnection('www.citilink.ru')
     c.request('GET',f'/catalog/noutbuki/?sorting=price_asc&f={choose_processor[0][money]},{choose_graphic_card[0][money]},{choose_storage_type[0][money]},{choose_ram_type[0][money]}')
     r = c.getresponse()
@@ -246,15 +245,42 @@ def choose():
     result = re.findall(r'alt=".+',html)
     for x in result[:-1]:
         st = f'\x1b[1m{x[5:-1]}\x1b[0m Процессор: {choose_processor[1][money]} Видеокарта: {choose_graphic_card[1][money]} Тип ОЗУ: {choose_ram_type[1][money]} Тип хранилища: {choose_storage_type[1][money]}'
-        settext(st,width/2-len(st)/2,height/2+indent)
+        settext(st,width/2,height/2+indent)
         indent += 1
     setoldflags()#set old flags
 
 #### MAIN ####
+cls()
+settext('Select language/Выберите язык',width/2,height/2,style=styles.bold)
+settext('English',width/3,height - height/4)#set exit text
+settext('Русский',width-width/3,height-height/4,style=styles.inverse)#set next text
+
+idx = 10001 #index of cursor
+
+while True:
+    c = getchar()
+    if c == '\x1b[D' or c == b'K': #LEFT ARROW
+        idx-=1
+    elif c == '\x1b[C' or c == b'M': #RIGHT ARROW
+        idx+=1
+    elif c == '\n' or c == b'\r': # ENTER KEY
+        if idx%2==1:#Continue
+            lang = 0
+            break
+        else:#Exit
+            lang = 1
+            break
+    if idx%2==1:
+        settext('English',width/3,height - height/4)
+        settext('Русский',(width-width/3),height-height/4,style=styles.inverse)
+    else:
+        settext('English',width/3,height - height/4,style=styles.inverse)
+        settext('Русский',(width-width/3),height-height/4)
+
 cls()#clear screen
 setsign(style=styles.bold)#set sign on screen
-settext('Выход',width/3-5,height - height/4)#set exit text
-settext('Далее',(width-width/3)-5,height-height/4,style=styles.inverse)#set next text
+settext(exitt[lang],width/3,height - height/4)#set exit text
+settext(nextt[lang],width-width/3,height-height/4,style=styles.inverse)#set next text
 
 idx = 10001 #index of cursor
 
@@ -272,17 +298,17 @@ while True:
             setoldflags()#set old flags
             exit(0)#exit from program
     if idx%2==1:
-        settext('Выход',width/3-5,height - height/4)
-        settext('Далее',(width-width/3)-5,height-height/4,style=styles.inverse)
+        settext(exitt[lang],width/3,height - height/4)#set exit text
+        settext(nextt[lang],width-width/3,height-height/4,style=styles.inverse)#set next text
     else:
-        settext('Выход',width/3-5,height - height/4,style=styles.inverse)
-        settext('Далее',(width-width/3)-5,height-height/4)
+        settext(exitt[lang],width/3,height - height/4,style=styles.inverse)#set exit text
+        settext(nextt[lang],width-width/3,height-height/4)#set next text
 
 cls()#clear screen
-settext('Выберите режим',width/2-7,height/2,style=styles.bold)
-settext('Подборка',width/3-5,height - height/4)
-settext('Конструктор',(width-width/3)-5,height-height/4,style=styles.inverse)
-settext('\x1b[1mКонструктор\x1b[0m - этот режим предназначен для создания своих параметров для ноутбука',width/2-40,height-height/5+2,style=styles.bold)
+settext(select_mode[lang],width/2,height/2,style=styles.bold)
+settext(chose[lang],width/3,height - height/4)
+settext(construct[lang],(width-width/3),height-height/4,style=styles.inverse)
+settext(construct_desc[lang],width/2,height-height/5+2)
 
 idx = 10001 #index of cursor
 
@@ -302,13 +328,13 @@ while True:
         break
     if idx%2==1:
         cls()#clear screen
-        settext('Выберите режим',width/2-7,height/2,style=styles.bold)
-        settext('Подборка',width/3-5,height - height/4)
-        settext('Конструктор',(width-width/3)-5,height-height/4,style=styles.inverse)
-        settext('\x1b[1mКонструктор\x1b[0m - этот режим предназначен для создания своих параметров для ноутбука',width/2-40,height-height/5+2,style=styles.bold)
+        settext(select_mode[lang],width/2,height/2,style=styles.bold)
+        settext(chose[lang],width/3,height - height/4)
+        settext(construct[lang],(width-width/3),height-height/4,style=styles.inverse)
+        settext(construct_desc[lang],width/2,height-height/5+2)
     else:
         cls()#clear screen
-        settext('Выберите режим',width/2-7,height/2,style=styles.bold)
-        settext('Подборка',width/3-5,height - height/4,style=styles.inverse)
-        settext('Конструктор',(width-width/3)-5,height-height/4)
-        settext('\x1b[1mПодборка\x1b[0m - этот режим предназначен для выбора ноутбука из уже существующих моделей',width/2-42,height-height/5+2,style=styles.bold)
+        settext(select_mode[lang],width/2,height/2,style=styles.bold)
+        settext(chose[lang],width/3,height - height/4,style=styles.inverse)
+        settext(construct[lang],(width-width/3),height-height/4)
+        settext(chose_desc[lang],width/2,height-height/5+2)
