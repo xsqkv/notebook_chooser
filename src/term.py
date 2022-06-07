2c329d90b8a2601167d077707333eb65430b5f1b
Artem Vanty <artem.vanty.0358@gmail.com>
2022-06-07 18:55:32 +0900

    add comments

--- src/term.py
+++ src/term.py
@@ -4,6 +4,7 @@ Header file for work with terminal
 
 from enum import IntEnum
 
+#enumeration of colors
 class colors(IntEnum):
     white = 37,
     yellow = 33,
@@ -14,11 +15,13 @@ class colors(IntEnum):
     magenta = 35,
     black = 30
 
+#enumeration of styles
 class styles(IntEnum):
     none = 0,
     bold = 1,
     underline = 4,
     inverse = 7
 
+#output function with styles
 def out(text,color,style,lst='\n'):
     print('\033[%d;%dm' % (style,color) + text + '\033[0m',end=lst)
